af8fd15a812e5b0ce979e782fe0419ac30cb74b1
murali_krishna <meruvamurali.ai@gmail.com>
2021-01-30 21:29:58 +0530

    updated dragdrop file and created a new file rightclickelement

--- src/test/java/TestCases/HandlingDragandDropElements.java
+++ src/test/java/TestCases/HandlingDragandDropElements.java
@@ -1,10 +1,51 @@
 package TestCases;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class HandlingDragandDropElements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		commit changes
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+		
+		System.out.println("No of Frames : " + frames.size());
+		
+		for(WebElement frame: frames) {
+			
+			if(frame.getAttribute("src").equals("https://jqueryui.com/resources/demos/droppable/default.html")) {
+				
+				driver.switchTo().frame(frame);
+				
+			}
+		}
+		
+		WebElement draggable = driver.findElement(By.id("draggable"));
+		WebElement droppable = driver.findElement(By.id("droppable"));
+			
+		Actions action = new Actions(driver);
+		action.dragAndDrop(draggable, droppable).perform();
+		
+		driver.switchTo().defaultContent();
+		
+		
+		
+		
 
 	}
 
---
+++ src/test/java/TestCases/RightClickingElement.java
@@ -0,0 +1,39 @@
+package TestCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RightClickingElement {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://deluxe-menu.com/popup-mode-sample.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		WebElement img = driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/div[2]/table[1]/tbody/tr/td[3]/p[2]/img"));
+		
+		Actions action = new Actions(driver);
+		action.contextClick(img).perform();
+		
+		WebElement productInfoMenu = driver.findElement(By.xpath("//*[@id=\"dm2m1i1tdT\"]"));
+		action.moveToElement(productInfoMenu).perform();
+		
+		WebElement installationMenu = driver.findElement(By.xpath("//*[@id=\"dm2m2i1tdT\"]"));
+		action.moveToElement(installationMenu).perform();
+		
+		driver.findElement(By.xpath("//*[@id=\"dm2m3i1tdT\"]")).click();
+
+	}
+
+}
