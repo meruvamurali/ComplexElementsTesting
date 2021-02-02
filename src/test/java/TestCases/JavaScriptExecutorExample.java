743a5dc1e13c8ad4b68cc389b0004ea36b15436c
murali_krishna <meruvamurali.ai@gmail.com>
2021-02-02 21:24:45 +0530

    Feb02

--- src/test/java/TestCases/JavaScriptExecutorExample.java
+++ src/test/java/TestCases/JavaScriptExecutorExample.java
@@ -1,20 +1,48 @@
 package TestCases;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class JavaScriptExecutorExample {
-
-	public static void main(String[] args) {
+	
+	public static WebDriver driver;
+	
+	public static void captureScreenshot() throws IOException {
+		
+		Date d = new Date();
+		String fileName = d.toString().replace(":", "_").replace(" ", "_")+ ".jpg";
+			
+		System.out.println(fileName);
+			
+		String folderName = "";
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MMM_dd");
+			
+		folderName = sdf.format(d);
+		System.out.println(folderName);
+			
+		File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshotFile, new File(".//screenshot//"+folderName+"//"+fileName));
+			
+		}
+	
+	public static void main(String[] args) throws IOException {
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		
 		driver.get("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_submit_get");
 		driver.manage().window().maximize();
@@ -30,6 +58,7 @@ public class JavaScriptExecutorExample {
 		
 		js.executeScript("arguments[0].style.border='3px solid red'", driver.findElement(By.id("mySubmit")));
 		
+		captureScreenshot();
 		
 
 	}
